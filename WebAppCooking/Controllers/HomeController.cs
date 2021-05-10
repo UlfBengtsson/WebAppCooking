3afaa0a3f4b208d365693413becbfcb77000e8b1
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-05-10 15:11:27 +0200

    Add project files.

---
+++ WebAppCooking/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppCooking.Models.Util;
+
+namespace WebAppCooking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ThisWeeks()
+        {
+            //result                 Use model to do logic work                        
+            List<string> menuPicks = RandomLunshGenerator.GenerateMenu();
+
+            //send result to view so it can show it to the user
+            return View(menuPicks);
+        }
+    }
+}
---
+++ WebAppCooking/Models/Util/RandomLunshGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCooking.Models.Util
+{
+    public class RandomLunshGenerator
+    {
+        private static Random randomGen = new Random();
+
+        public static List<string> menuOptions = new List<string>()
+        {
+            "Chicken with Rise",
+            "Fried patatos with fish",
+            "Muschroom stew",
+            "Swedish meatballs with potatos",
+            "Fufu",
+            "Pizza",
+            "Sallad blow",
+            "Spaggeti bolones",
+            "Hamburger"
+        };
+
+        public static List<string> GenerateMenu()
+        {
+            List<string> weekPicks = new List<string>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                weekPicks.Add(
+                    menuOptions[randomGen.Next(menuOptions.Count)]
+                    );
+            }
+
+            return weekPicks;
+        }
+    }
+}
